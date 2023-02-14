b157d4af07e1544f5c239c31031b450b6a233e3b
AYC93 <ayc.leo@gmail.com>
2023-02-14 16:50:11 +0800

    task02 6th draft

--- task02/src/main/java/sdfday10_assessment_t2/App.java
+++ task02/src/main/java/sdfday10_assessment_t2/App.java
@@ -7,9 +7,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public final class App {
     private App() {
@@ -24,8 +26,9 @@ public final class App {
         File fileInt = new File(dirName);
         File[] fileArr = fileInt.listFiles();
 
-        Map<String, Integer> wordList = new LinkedHashMap<String, Integer>();
+        Map<String, Integer> wordMap = new LinkedHashMap<String, Integer>();
         List<String> fileList = new ArrayList<String>();
+        Set<String> wordSet = new HashSet<>();
 
         for (int i = 0; i < fileArr.length; i++){
             fileList.add(fileArr[i].getName());
@@ -41,11 +44,13 @@ public final class App {
 
         String sent = "";
 
+        
         for (int j = 0; j < fileNameList.size(); j++){
             fileName = fileNameList.get(j);
             File fileFin = new File(dirName, fileName);
-            System.out.println("\r");
+            System.out.println("\n");
             System.out.println(fileFin);
+            System.out.println("\n");
             FileReader fr = new FileReader(fileFin);
             BufferedReader br = new BufferedReader(fr);
 
@@ -54,23 +59,22 @@ public final class App {
                     String strippedSent = sent.replaceAll("[^a-zA-Z ]", "").toLowerCase();
                     String[] wordArr = strippedSent.split("\\s+");
                     for (int i = 0; i < wordArr.length; i++){
-                        wordList.put(wordArr[i], i);
+                        wordMap.put(wordArr[i], i);
+                        wordSet.add(wordArr[i]);
                     }
                 }     
+
                 br.close();
-                System.out.println(wordList);
+                System.out.println(wordMap);
+                System.out.println(wordSet);
 
-                // wordList as an array
+                HashMap<Integer,String> uniqSenStr = new HashMap<>();
 
-            HashMap<String, Integer> uniqSenStr = new HashMap<>();
-
-            for (j =0; j < wordList.size() - 1 ; j++){
-                String shortStr = wordList.get(j) + " " + wordList.get(j+1);
-                uniqSenStr.put(shortStr, j);
+                for (j =0; j < wordMap.size() - 1 ; j++){
+                String shortStr = wordMap.get(j) + " " + wordMap.get(j+1);
+                uniqSenStr.put(j, shortStr);
+             }
+                // System.out.println(uniqSenStr);
+            }
+      }
     }
-                System.out.println(uniqSenStr);
-}
-    }
-}
-
-    
