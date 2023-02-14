6c482fdc29939d42708ef05e33f50fca174c0ea5
AYC93 <ayc.leo@gmail.com>
2023-02-14 14:58:08 +0800

    task02 4th draft

--- task02/src/main/java/sdfday10_assessment_t2/App.java
+++ task02/src/main/java/sdfday10_assessment_t2/App.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class App {
     private App() {
@@ -55,19 +57,19 @@ public final class App {
                 br.close();
                 System.out.println(wordList);
 
-            // List<String,Integer> uniqSentString = new ArrayList<String>();
+            Map<String,Integer> uniqSentString = new HashMap<>();
 
-            // for (int j=0; j <wordList.size();j++){
-            //     int count;
-            //     String shortStr = wordList.get(j) + " " + wordList.get(j+1);
-            //     uniqSentString.add(shortStr);
-            //     for (int k = 0; k < uniqSentString.size(); k++){
-            //         if(uniqSentString[k].matches(shortStr)){
-            //             count++;
-            //             System.out.println(count);
-            //         }
-            //     }
-            // }
+            for (int j=0; j <wordList.size();j++){
+                int count;
+                String shortStr = wordList.get(j) + " " + wordList.get(j+1);
+                uniqSentString.put(shortStr, j);
+                for (int k = 0; k < uniqSentString.size(); k++){
+                    if(uniqSentString.get(k).equals(shortStr)){
+                        count++;
+                        System.out.println(count);
+                    }
+                }
+            }
     }
 }
 }
