64d532d49c202d08b8764e1fcb4e76fe0d4a2000
AYC93 <ayc.leo@gmail.com>
2023-02-14 12:20:22 +0800

    task02 middle draft

--- task02/src/main/java/sdfday10_assessment_t2/App.java
+++ task02/src/main/java/sdfday10_assessment_t2/App.java
@@ -11,27 +11,61 @@ import java.util.List;
 public final class App {
     private App() {
     }
-
+    public static String dirName = "seuss";
+    public static String fileName = "";
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        String dirName;
-        
-        
-        File file = new File(dirName);
-        FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
+          
+        File fileInt = new File(dirName);
+        File[] fileArr = fileInt.listFiles();
 
         List<String> wordList = new ArrayList<String>();
+        List<String> fileList = new ArrayList<String>();
 
-        String word = "";
+        for (int i = 0; i < fileArr.length; i++){
+            fileList.add(fileArr[i].getName());
+            System.out.println(fileList);
+        } 
 
-        while((word = br.readLine())!=null){
-            String strippedWord = word.replaceAll("[^a-zA-Z ]", "").toLowerCase();
-            wordList.add(strippedWord);
+        // file names of all 4 txt, checked and working
+        String file1 = fileList.get(0);
+        String file2 = fileList.get(1);
+        String file3 = fileList.get(2);
+        String file4 = fileList.get(3);
+
+        String sent = "";
+
+
+        //file 1
+        fileName = file1;
+
+        File fileFin = new File(dirName, fileName);
+        System.out.println(fileFin);
+        FileReader fr = new FileReader(fileFin);
+        BufferedReader br = new BufferedReader(fr);
+
+        // strip all punctuations
+        while((sent = br.readLine())!= null){
+                String strippedSent = sent.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+                String[] wordArr = strippedSent.split("\\s+");
+                for (int i = 0; i < wordArr.length; i++){
+                    wordList.add(wordArr[i]);
+                }
+             }     
+            br.close();
+            System.out.println(wordList);
+
+        Map<String,Integer> uniqSentString = new HashMap<String>();
+
+        for (int j=0; j <wordList.size();j++){
+            int count;
+            String shortStr = wordList.get(j) + " " + wordList.get(j+1);
+            uniqSentString.add(shortStr);
+            for (int k = 0; k<uniqSentString.size(); k++){
+                if(!uniqSentString..equals(shortStr)){
+                    count++;
+                }
+            }
         }
-
-        br.close();
-
-        System.out.println(wordList);
     }
-}
+    
