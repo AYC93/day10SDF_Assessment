4966a9380a5f8d8fea88867ab62c7d60bcd8a050
AYC93 <ayc.leo@gmail.com>
2023-02-14 15:24:52 +0800

    task02 4th draft

--- task02/src/main/java/sdfday10_assessment_t2/App.java
+++ task02/src/main/java/sdfday10_assessment_t2/App.java
@@ -7,8 +7,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public final class App {
     private App() {
@@ -17,11 +19,13 @@ public final class App {
     public static String fileName = "";
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-          
+        
+        dirName = args[0];
+
         File fileInt = new File(dirName);
         File[] fileArr = fileInt.listFiles();
 
-        List<String> wordList = new ArrayList<String>();
+        Map<String, Integer> wordList = new HashMap<String, Integer>();
         List<String> fileList = new ArrayList<String>();
 
         for (int i = 0; i < fileArr.length; i++){
@@ -51,26 +55,27 @@ public final class App {
                     String strippedSent = sent.replaceAll("[^a-zA-Z ]", "").toLowerCase();
                     String[] wordArr = strippedSent.split("\\s+");
                     for (int i = 0; i < wordArr.length; i++){
-                        wordList.add(wordArr[i]);
+                        wordList.put(wordArr[i], i);
                     }
                 }     
                 br.close();
                 System.out.println(wordList);
 
-            Map<String,Integer> uniqSentString = new HashMap<>();
+                // wordList as an array
 
-            for (int j=0; j <wordList.size();j++){
-                int count;
+            Set<String> uniqSentString = new HashSet<>();
+
+            for (j =0; j < wordList.size() ; j++){
                 String shortStr = wordList.get(j) + " " + wordList.get(j+1);
                 uniqSentString.put(shortStr, j);
                 for (int k = 0; k < uniqSentString.size(); k++){
-                    if(uniqSentString.get(k).equals(shortStr)){
-                        count++;
-                        System.out.println(count);
-                    }
-                }
-            }
+            //         if(uniqSentString.get(k).equals(shortStr)){
+            //             count++;
+            //             System.out.println(count);
+            //         }
+            //     }
+            // }
     }
-}
-}
+}}
+
     
